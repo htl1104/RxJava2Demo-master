58eab56fd88ebd32a9787078d298b2f3193b54ec
Administrator <2331464655@qq.com>
2018-01-12 15:31:22 +0800

    Initial commit

---
+++ app/src/main/java/zlc/season/rxjava2demo/MainActivity.java
@@ -0,0 +1,48 @@
+package zlc.season.rxjava2demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import java.io.InterruptedIOException;
+
+import io.reactivex.functions.Consumer;
+import io.reactivex.plugins.RxJavaPlugins;
+import zlc.season.rxjava2demo.demo.ChapterNine;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "TAG";
+
+    static {
+        RxJavaPlugins.setErrorHandler(new Consumer<Throwable>() {
+            @Override
+            public void accept(Throwable throwable) throws Exception {
+                if (throwable instanceof InterruptedIOException) {
+                    Log.d(TAG, "Io interrupted");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ChapterNine.demo1();
+            }
+        });
+
+        findViewById(R.id.request).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChapterNine.request(96);
+            }
+        });
+    }
+
+}
